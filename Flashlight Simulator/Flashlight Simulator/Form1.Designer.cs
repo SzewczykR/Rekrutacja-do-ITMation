ddf9482ffd045551fd80aaf47bb4d1de7fff7280
SzewczykR <szewczykry@gmail.com>
2018-04-25 05:11:32 +0200

    Flashlight Simulator

---
+++ Flashlight Simulator/Flashlight Simulator/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Flashlight_Simulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnBatteryIn = new System.Windows.Forms.Button();
+            this.btnBatteryOut = new System.Windows.Forms.Button();
+            this.btnFlashlightOn = new System.Windows.Forms.Button();
+            this.btnFlashlightOff = new System.Windows.Forms.Button();
+            this.btnBulbIn = new System.Windows.Forms.Button();
+            this.btnBulbChange = new System.Windows.Forms.Button();
+            this.tbFlashlightState = new System.Windows.Forms.TextBox();
+            this.tbBulbStrength = new System.Windows.Forms.TextBox();
+            this.tbBattery1State = new System.Windows.Forms.TextBox();
+            this.tbBattery2State = new System.Windows.Forms.TextBox();
+            this.tbBattery3State = new System.Windows.Forms.TextBox();
+            this.tbBattery4State = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.prgBattery1 = new System.Windows.Forms.ProgressBar();
+            this.prgBattery2 = new System.Windows.Forms.ProgressBar();
+            this.prgBattery3 = new System.Windows.Forms.ProgressBar();
+            this.prgBattery4 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnBatteryIn
+            // 
+            this.btnBatteryIn.Location = new System.Drawing.Point(12, 94);
+            this.btnBatteryIn.Name = "btnBatteryIn";
+            this.btnBatteryIn.Size = new System.Drawing.Size(96, 23);
+            this.btnBatteryIn.TabIndex = 0;
+            this.btnBatteryIn.Text = "Włóż baterie";
+            this.btnBatteryIn.UseVisualStyleBackColor = true;
+            this.btnBatteryIn.Click += new System.EventHandler(this.btnBatteryIn_Click);
+            // 
+            // btnBatteryOut
+            // 
+            this.btnBatteryOut.Location = new System.Drawing.Point(12, 123);
+            this.btnBatteryOut.Name = "btnBatteryOut";
+            this.btnBatteryOut.Size = new System.Drawing.Size(96, 23);
+            this.btnBatteryOut.TabIndex = 1;
+            this.btnBatteryOut.Text = "Wyjmij baterię";
+            this.btnBatteryOut.UseVisualStyleBackColor = true;
+            this.btnBatteryOut.Click += new System.EventHandler(this.btnBatteryOut_Click);
+            // 
+            // btnFlashlightOn
+            // 
+            this.btnFlashlightOn.Location = new System.Drawing.Point(12, 12);
+            this.btnFlashlightOn.Name = "btnFlashlightOn";
+            this.btnFlashlightOn.Size = new System.Drawing.Size(96, 23);
+            this.btnFlashlightOn.TabIndex = 2;
+            this.btnFlashlightOn.Text = "Włącz latarkę";
+            this.btnFlashlightOn.UseVisualStyleBackColor = true;
+            this.btnFlashlightOn.Click += new System.EventHandler(this.btnFlashlightOn_Click);
+            // 
+            // btnFlashlightOff
+            // 
+            this.btnFlashlightOff.Location = new System.Drawing.Point(12, 41);
+            this.btnFlashlightOff.Name = "btnFlashlightOff";
+            this.btnFlashlightOff.Size = new System.Drawing.Size(96, 23);
+            this.btnFlashlightOff.TabIndex = 3;
+            this.btnFlashlightOff.Text = "Wyłącz latarkę";
+            this.btnFlashlightOff.UseVisualStyleBackColor = true;
+            this.btnFlashlightOff.Click += new System.EventHandler(this.btnFlashlightOff_Click);
+            // 
+            // btnBulbIn
+            // 
+            this.btnBulbIn.Location = new System.Drawing.Point(12, 176);
+            this.btnBulbIn.Name = "btnBulbIn";
+            this.btnBulbIn.Size = new System.Drawing.Size(96, 23);
+            this.btnBulbIn.TabIndex = 4;
+            this.btnBulbIn.Text = "Włóż żarówkę";
+            this.btnBulbIn.UseVisualStyleBackColor = true;
+            this.btnBulbIn.Click += new System.EventHandler(this.btnBulbIn_Click);
+            // 
+            // btnBulbChange
+            // 
+            this.btnBulbChange.Location = new System.Drawing.Point(12, 205);
+            this.btnBulbChange.Name = "btnBulbChange";
+            this.btnBulbChange.Size = new System.Drawing.Size(96, 23);
+            this.btnBulbChange.TabIndex = 5;
+            this.btnBulbChange.Text = "Wymień żarówkę";
+            this.btnBulbChange.UseVisualStyleBackColor = true;
+            this.btnBulbChange.Click += new System.EventHandler(this.btnBulbChange_Click);
+            // 
+            // tbFlashlightState
+            // 
+            this.tbFlashlightState.BackColor = System.Drawing.Color.Red;
+            this.tbFlashlightState.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbFlashlightState.Location = new System.Drawing.Point(251, 29);
+            this.tbFlashlightState.Name = "tbFlashlightState";
+            this.tbFlashlightState.ReadOnly = true;
+            this.tbFlashlightState.Size = new System.Drawing.Size(100, 20);
+            this.tbFlashlightState.TabIndex = 6;
+            this.tbFlashlightState.Text = "WYŁĄCZONA";
+            this.tbFlashlightState.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbBulbStrength
+            // 
+            this.tbBulbStrength.BackColor = System.Drawing.Color.White;
+            this.tbBulbStrength.Location = new System.Drawing.Point(251, 55);
+            this.tbBulbStrength.Name = "tbBulbStrength";
+            this.tbBulbStrength.ReadOnly = true;
+            this.tbBulbStrength.Size = new System.Drawing.Size(100, 20);
+            this.tbBulbStrength.TabIndex = 7;
+            this.tbBulbStrength.Text = "brak żarówki";
+            this.tbBulbStrength.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbBulbStrength.TextChanged += new System.EventHandler(this.tbBulbStrength_TextChanged);
+            // 
+            // tbBattery1State
+            // 
+            this.tbBattery1State.Location = new System.Drawing.Point(221, 94);
+            this.tbBattery1State.Name = "tbBattery1State";
+            this.tbBattery1State.ReadOnly = true;
+            this.tbBattery1State.Size = new System.Drawing.Size(24, 20);
+            this.tbBattery1State.TabIndex = 8;
+            // 
+            // tbBattery2State
+            // 
+            this.tbBattery2State.Location = new System.Drawing.Point(221, 123);
+            this.tbBattery2State.Name = "tbBattery2State";
+            this.tbBattery2State.ReadOnly = true;
+            this.tbBattery2State.Size = new System.Drawing.Size(24, 20);
+            this.tbBattery2State.TabIndex = 9;
+            // 
+            // tbBattery3State
+            // 
+            this.tbBattery3State.Location = new System.Drawing.Point(221, 152);
+            this.tbBattery3State.Name = "tbBattery3State";
+            this.tbBattery3State.ReadOnly = true;
+            this.tbBattery3State.Size = new System.Drawing.Size(24, 20);
+            this.tbBattery3State.TabIndex = 10;
+            // 
+            // tbBattery4State
+            // 
+            this.tbBattery4State.Location = new System.Drawing.Point(221, 181);
+            this.tbBattery4State.Name = "tbBattery4State";
+            this.tbBattery4State.ReadOnly = true;
+            this.tbBattery4State.Size = new System.Drawing.Size(24, 20);
+            this.tbBattery4State.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(166, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Stan latarki";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(166, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Moc żarówki";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(166, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Bateria 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(166, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Bateria 2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(166, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Bateria 3";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(166, 184);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Bateria 4";
+            // 
+            // prgBattery1
+            // 
+            this.prgBattery1.Location = new System.Drawing.Point(251, 94);
+            this.prgBattery1.Name = "prgBattery1";
+            this.prgBattery1.Size = new System.Drawing.Size(100, 23);
+            this.prgBattery1.TabIndex = 19;
+            // 
+            // prgBattery2
+            // 
+            this.prgBattery2.Location = new System.Drawing.Point(251, 123);
+            this.prgBattery2.Name = "prgBattery2";
+            this.prgBattery2.Size = new System.Drawing.Size(100, 23);
+            this.prgBattery2.TabIndex = 20;
+            // 
+            // prgBattery3
+            // 
+            this.prgBattery3.Location = new System.Drawing.Point(251, 152);
+            this.prgBattery3.Name = "prgBattery3";
+            this.prgBattery3.Size = new System.Drawing.Size(100, 23);
+            this.prgBattery3.TabIndex = 21;
+            // 
+            // prgBattery4
+            // 
+            this.prgBattery4.Location = new System.Drawing.Point(251, 181);
+            this.prgBattery4.Name = "prgBattery4";
+            this.prgBattery4.Size = new System.Drawing.Size(100, 23);
+            this.prgBattery4.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 241);
+            this.Controls.Add(this.prgBattery4);
+            this.Controls.Add(this.prgBattery3);
+            this.Controls.Add(this.prgBattery2);
+            this.Controls.Add(this.prgBattery1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbBattery4State);
+            this.Controls.Add(this.tbBattery3State);
+            this.Controls.Add(this.tbBattery2State);
+            this.Controls.Add(this.tbBattery1State);
+            this.Controls.Add(this.tbBulbStrength);
+            this.Controls.Add(this.tbFlashlightState);
+            this.Controls.Add(this.btnBulbChange);
+            this.Controls.Add(this.btnBulbIn);
+            this.Controls.Add(this.btnFlashlightOff);
+            this.Controls.Add(this.btnFlashlightOn);
+            this.Controls.Add(this.btnBatteryOut);
+            this.Controls.Add(this.btnBatteryIn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Flashlight Simulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBatteryIn;
+        private System.Windows.Forms.Button btnBatteryOut;
+        private System.Windows.Forms.Button btnFlashlightOn;
+        private System.Windows.Forms.Button btnFlashlightOff;
+        private System.Windows.Forms.Button btnBulbIn;
+        private System.Windows.Forms.Button btnBulbChange;
+        private System.Windows.Forms.TextBox tbFlashlightState;
+        private System.Windows.Forms.TextBox tbBulbStrength;
+        private System.Windows.Forms.TextBox tbBattery1State;
+        private System.Windows.Forms.TextBox tbBattery2State;
+        private System.Windows.Forms.TextBox tbBattery3State;
+        private System.Windows.Forms.TextBox tbBattery4State;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ProgressBar prgBattery1;
+        private System.Windows.Forms.ProgressBar prgBattery2;
+        private System.Windows.Forms.ProgressBar prgBattery3;
+        private System.Windows.Forms.ProgressBar prgBattery4;
+    }
+}
+
---
+++ Flashlight Simulator/Flashlight Simulator/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Flashlight_Simulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBatteryIn_Click(object sender, EventArgs e)
+        {
+            if (Flashlight.GetBatteriesInCount() < 4)
+            {
+                Flashlight.PlugInBattery();
+                switch (Flashlight.GetBatteriesInCount())
+                {
+                    case 1:
+                        tbBattery1State.Text = Flashlight.GetPowerFromBatteriesIn(0).ToString();
+                        prgBattery1.Value = Flashlight.GetPowerFromBatteriesIn(0);
+                        break;
+                    case 2:
+                        tbBattery2State.Text = Flashlight.GetPowerFromBatteriesIn(1).ToString();
+                        prgBattery2.Value = Flashlight.GetPowerFromBatteriesIn(1);
+                        break;
+                    case 3:
+                        tbBattery3State.Text = Flashlight.GetPowerFromBatteriesIn(2).ToString();
+                        prgBattery3.Value = Flashlight.GetPowerFromBatteriesIn(2);
+                        break;
+                    case 4:
+                        tbBattery4State.Text = Flashlight.GetPowerFromBatteriesIn(3).ToString();
+                        prgBattery4.Value = Flashlight.GetPowerFromBatteriesIn(3);
+                        break;
+                }
+            }
+            else
+                MessageBox.Show("Nie ma miejsca na kolejne baterie!", "Błąd!");
+        }
+
+        private void btnFlashlightOn_Click(object sender, EventArgs e)
+        {
+            if (Flashlight.GetBulbState() && Flashlight.GetBatteriesInCount() >= 2 && Flashlight.IsEnoughPower())
+            {
+                for(int i=0; i < Flashlight.GetBatteriesInCount(); i++)
+                {
+
+                    Flashlight.ConsumePowerFromBatteriesIn(i);
+                        switch (i + 1)
+                        {
+                            case 1:
+                                tbBattery1State.Text = Flashlight.GetPowerFromBatteriesIn(i).ToString();
+                                prgBattery1.Value = Flashlight.GetPowerFromBatteriesIn(i);
+                                break;
+                            case 2:
+                                tbBattery2State.Text = Flashlight.GetPowerFromBatteriesIn(i).ToString();
+                                prgBattery2.Value = Flashlight.GetPowerFromBatteriesIn(i);
+                                break;
+                            case 3:
+                                tbBattery3State.Text = Flashlight.GetPowerFromBatteriesIn(i).ToString();
+                                prgBattery3.Value = Flashlight.GetPowerFromBatteriesIn(i);
+                                break;
+                            case 4:
+                                tbBattery4State.Text = Flashlight.GetPowerFromBatteriesIn(i).ToString();
+                                prgBattery4.Value = Flashlight.GetPowerFromBatteriesIn(i);
+                                break;
+                    }
+                }
+                tbFlashlightState.Text = "WŁĄCZONA";
+                tbFlashlightState.BackColor = Color.Green;
+                Flashlight.TurnOn();
+            }
+            else
+            {
+                if (!Flashlight.GetBulbState() && Flashlight.GetBatteriesInCount() < 2)
+                    MessageBox.Show("W latarce nie ma żarówki ani dostatecznej ilości baterii.", "Błąd!");
+                else if (!Flashlight.GetBulbState() && !Flashlight.IsEnoughPower())
+                    MessageBox.Show("W latarce nie ma żarówki ani dostatecznej ilości mocy w bateriach.", "Błąd!");
+                else if (!Flashlight.GetBulbState())
+                    MessageBox.Show("W latarce nie ma żarówki!", "Błąd!");
+                else if (Flashlight.GetBatteriesInCount() < 2)
+                    MessageBox.Show("W latarce nie ma dostatecznej ilości baterii.", "Błąd!");
+                else
+                    MessageBox.Show("W latarce nie ma dostatecznej ilości mocy.", "Błąd!");
+            }
+        }
+
+        private void btnFlashlightOff_Click(object sender, EventArgs e)
+        {
+            Flashlight.TurnOff();
+            tbFlashlightState.Text = "WYŁĄCZONA";
+            tbFlashlightState.BackColor = Color.Red;
+        }
+
+        private void btnBulbIn_Click(object sender, EventArgs e)
+        {
+            Flashlight.PlugInBulb();
+            tbBulbStrength.Text = Bulb.GetStrength().ToString();
+            tbBulbStrength.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(230)))));
+        }
+
+        private void btnBulbChange_Click(object sender, EventArgs e)
+        {
+            if (Flashlight.GetBulbState())
+            {
+                string inputbox = Interaction.InputBox("Podaj moc nowej żaróweczki [1-10]", "Wymieniamy żaróweczkę", Bulb.GetStrength().ToString(), -1, -1);
+                int output;
+                bool isInt = Int32.TryParse(inputbox, out output);
+                if (isInt && output >= 1 && output <= 10)
+                {
+                    Bulb.SetStrength(output);
+                    tbBulbStrength.Text = output.ToString();
+                    tbBulbStrength.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255-output*25)))));
+                }
+                else
+                    MessageBox.Show("Zła wartość!", "Błąd!");
+            }
+            else MessageBox.Show("Najpierw wsadź żarówkę!", "Błąd!");
+        }
+
+        private void btnBatteryOut_Click(object sender, EventArgs e)
+        {
+            if (Flashlight.GetBatteriesInCount() > 0)
+            {
+                Flashlight.RemoveBattery();
+                switch (Flashlight.GetBatteriesInCount()+1)
+                {
+                    case 1:
+                        tbBattery1State.Text = null;
+                        prgBattery1.Value = 0;
+                        break;
+                    case 2:
+                        tbBattery2State.Text = null;
+                        prgBattery2.Value = 0;
+                        break;
+                    case 3:
+                        tbBattery3State.Text = null;
+                        prgBattery3.Value = 0;
+                        break;
+                    case 4:
+                        tbBattery4State.Text = null;
+                        prgBattery4.Value = 0;
+                        break;
+                }
+            }
+            else
+                MessageBox.Show("Brak baterii w latarce!", "Błąd!");
+        }
+
+        static class Flashlight
+        {
+            private static List<Battery> BatteriesIn = new List<Battery>();
+            private static List<Battery> BatteriesOut = new List<Battery>();
+            private static bool isOn;
+            private static bool isBulbIn;
+            static Flashlight()
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    Battery battery = new Battery(rnd.Next(90,100));
+                    BatteriesOut.Add(battery);
+                }
+                isOn = false;
+                isBulbIn = false;
+            }
+            public static void TurnOn()
+            {
+                isOn = true;
+            }
+            public static void TurnOff()
+            {
+                isOn = false;
+            }
+            public static void PlugInBulb()
+            {
+                isBulbIn = true;
+            }
+            public static void SwitchBulb(int strength)
+            {
+                Bulb.SetStrength(strength);
+            }
+            public static void RemoveBattery()
+            {
+                BatteriesOut.Add(BatteriesIn.ElementAt(BatteriesIn.Count-1));
+                BatteriesIn.RemoveAt(BatteriesIn.Count - 1);
+            }
+            public static void PlugInBattery()
+            {
+                BatteriesIn.Add(BatteriesOut.ElementAt(0));
+                BatteriesOut.RemoveAt(0);
+            }
+            public static int GetBatteriesInCount()
+            {
+                return BatteriesIn.Count;
+            }
+            public static bool GetBulbState()
+            {
+                return isBulbIn;
+            }
+            public static bool IsEnoughPower()
+            {
+                int i = 0;
+                foreach (Battery battery in BatteriesIn)
+                {
+                    if (battery.GetPower() >= 15)
+                        i++;
+                }
+                if (i >= 2)
+                    return true;
+                else
+                    return false;
+            }
+            public static int GetPowerFromBatteriesIn(int index)
+            {
+                return BatteriesIn.ElementAt(index).GetPower();
+            }
+            public static void ConsumePowerFromBatteriesIn(int index)
+            {
+                BatteriesIn.ElementAt(index).ConsumePower(Bulb.GetStrength());
+                if (BatteriesIn.ElementAt(index).GetPower() < 0)
+                   BatteriesIn.ElementAt(index).SetPower(0);
+            }
+        }
+
+        static class Bulb
+        {
+            private static int strength;
+            static Bulb()
+            {
+                strength = 1;
+            }
+            public static void SetStrength(int sstrength)
+            {
+                strength = sstrength;
+            }
+            public static int GetStrength()
+            {
+                return strength;
+            }
+        }
+
+        class Battery
+        {
+            private int power;
+            public Battery(int power)
+            {
+                this.power = power;
+            }
+            public void ConsumePower(int strength)
+            {
+                this.power -= strength;
+            }
+            public int GetPower()
+            {
+                return power;
+            }
+            public void SetPower(int ppower)
+            {
+                power = ppower;
+            }
+        }
+
+        private void tbBulbStrength_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
